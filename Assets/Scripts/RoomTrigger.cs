1e9fea0059ef0b7b5ebe9afe86e54c6e93f4d14b
crazyjackel <47047592+crazyjackel@users.noreply.github.com>
2019-01-26 11:12:02 -0500

    Update RoomTrigger.cs

--- Assets/Scripts/RoomTrigger.cs
+++ Assets/Scripts/RoomTrigger.cs
@@ -1,30 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RoomTrigger : MonoBehaviour
 {
+    public UnityEvent EnterEvent;
+    public UnityEvent ExitEvent;
     public Enemy[] enemies;
+    int numEnemies;
+    private bool active;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (active)
+        {
+            int count = 0;
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                if (enemies[i] != null)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                ExitEvent.Invoke();
+            }
+            numEnemies = count;
+        }
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            foreach (Enemy enemy in enemies)
-            {
-                enemy.chase = true;
-            }
+            Enter();
         }
 
 
@@ -38,6 +55,16 @@ public class RoomTrigger : MonoBehaviour
             {
                 enemy.chase = false;
             }
+        }
+    }
+
+    private void Enter()
+    {
+        EnterEvent.Invoke();
+        active = true;
+        foreach (Enemy enemy in enemies)
+        {
+            enemy.chase = true;
         }
     }
 }
