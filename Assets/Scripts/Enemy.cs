1911b3d6a74134839e1336f8b5ce22aa4a3fd590
crazyjackel <47047592+crazyjackel@users.noreply.github.com>
2019-01-26 08:55:00 -0500

    First work on Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Collider HitBox;
+    public int health;
     // Start is called before the first frame update
     void Start()
     {
-        
+        HitBox = this.GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 0)
+        {
+            Destroy(this);
+        }
     }
 }
