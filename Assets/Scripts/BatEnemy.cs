cf472442a7717a56f369ded5703a6caeb8ed47d3
crazyjackel <47047592+crazyjackel@users.noreply.github.com>
2019-01-26 10:00:04 -0500

    Batenemy

---
+++ Assets/Scripts/BatEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatEnemy : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 EnemyPosition = this.transform.position;
+        Vector3 position = DataManager.Instance.Player.transform.position;
+        Vector2 CartesianPosition = Vector2.Lerp(new Vector2(EnemyPosition.x,EnemyPosition.z),new Vector2(position.x,position.z),Time.deltaTime);
+        this.transform.position = new Vector3(CartesianPosition.x,EnemyPosition.y,CartesianPosition.y);
+    }
+}
