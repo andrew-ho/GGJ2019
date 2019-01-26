d03898448e155bce4dbf868eed1a622d6cf2620a
Andrew <aho3555@gmail.com>
2019-01-26 18:16:36 -0500

    health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image image;
+    public float fill;
+
+    Movement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fill = player.characterHealth / 50f;
+        image.fillAmount = fill;
+    }
+}
