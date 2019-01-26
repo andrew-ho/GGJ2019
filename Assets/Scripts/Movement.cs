845609bd3cd4629115e91a8a3104812cf52ef26f
Andrew <aho3555@gmail.com>
2019-01-25 23:55:46 -0500

    meh

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody rb;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Debug.Log(moveInput);
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+    }
+}
