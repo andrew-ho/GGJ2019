20244d5b91aefb50e72b5f493798c33ad3b4bb3f
Andrew <aho3555@gmail.com>
2019-01-27 14:41:47 -0500

    easter egg

--- Assets/Scripts/EasterEgg.cs
+++ Assets/Scripts/EasterEgg.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EasterEgg : MonoBehaviour
 {
+    float time;
+    public TextMeshProUGUI text;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class EasterEgg : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            text.gameObject.SetActive(true);
+        }
     }
 }
