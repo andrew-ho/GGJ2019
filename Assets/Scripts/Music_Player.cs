1435e309439e64425e059af79c10d7a6ceb19eef
Jacob Light <jakestar0306@comcast.net>
2019-01-27 14:28:27 -0500

    Added music stuff

---
+++ Assets/Scripts/Music_Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Player : MonoBehaviour {
+
+	public AudioClip otherClip;
+    AudioSource audioSource;
+
+    public AudioClip thirdClip;
+    bool fightingBoss = false;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!fightingBoss && !audioSource.isPlaying) {
+            audioSource.clip = otherClip;
+            audioSource.Play();
+            audioSource.loop = true;
+        }
+	}
+
+	public void StartBossMusic() {
+		if (!fightingBoss) {
+			fightingBoss = true;
+			audioSource.clip = thirdClip;
+	        audioSource.Play();
+	        audioSource.loop = true;
+		}
+	}
+}
