8b5bbca8e5cc545da56b70bc96d349fe137edc0d
Andrew <aho3555@gmail.com>
2019-01-27 03:48:34 -0500

    stuff

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timer;
+    public TextMeshProUGUI text;
+    float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        text.text = timer.ToString();
+    }
+}
